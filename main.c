b34658b963879426a3ad13d1302436d5ce70252f
Pratiksha1411 <97377782+Pratiksha1411@users.noreply.github.com>
2022-02-09 09:22:49 +0530

    local file
    adding local file to repository

---
+++ main.c
@@ -0,0 +1,19 @@
+main()
+{
+    int num;
+    printf("enter the number");
+    scanf("%d",&num);
+    if(num>0)
+    {
+        printf("number is positive");
+    }
+    if(num<0)
+    {
+        printf("number is negative");
+    }
+    if(num==0)
+    {
+        printf("number is zero");
+    }
+    getch();
+}
